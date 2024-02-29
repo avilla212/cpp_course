8a9ca16591d244552dd871dc909dc7de776d77af
avilla212 <alexvillafana99@gmail.com>
2024-02-29 13:07:07 -0800

    2/29 mac

--- test_mac.cpp
+++ test_mac.cpp
@@ -2,55 +2,37 @@
 
 using namespace std;
 
-// Define your own value for pi
-const double PI = 3.14;
-
-// Function to calculate the area of a circle
-double calculateCircleArea(double diameter) {
-    double radius = diameter / 2.0;
-    return PI * radius * radius;
-}
-
-// Function to calculate the area of a rectangle
-double calculateRectangleArea(double length, double width) {
-    return length * width;
+// Function to calculate power
+double calculatePower(double base, int exponent) {
+    double result = 1.0;
+    for (int i = 0; i < exponent; ++i) {
+        result *= base;
+    }
+    return result;
 }
 
 int main() {
-    double diameter, length, width;
-    int choice;
+    double base;
+    int exponent;
 
-    do {
-        // Prompt user for circle diameter and rectangle dimensions
-        cout << "Enter the diameter of the circle: ";
-        cin >> diameter;
-        cout << "Enter the length of the rectangle: ";
-        cin >> length;
-        cout << "Enter the width of the rectangle: ";
-        cin >> width;
+    // Loop until the user enters 0 for the base
+    while (true) {
+        cout << "Enter the base (or 0 to exit): ";
+        cin >> base;
 
-        // Calculate areas
-        double circleArea = calculateCircleArea(diameter);
-        double rectangleArea = calculateRectangleArea(length, width);
-
-        // Display results
-        cout << "The circle with diameter " << diameter << " has an area of " << circleArea << endl;
-        cout << "The rectangle with a length of " << length << " and a width of " << width << " has an area of " << rectangleArea << endl;
-
-        // Compare areas and display comparison
-        if (circleArea > rectangleArea) {
-            cout << "The circle's area is " << circleArea / rectangleArea << " times the area of rectangle" << endl;
-        } else {
-            cout << "The rectangle's area is " << rectangleArea / circleArea << " times the area of circle" << endl;
+        // Check for sentinel value
+        if (base == 0) {
+            cout << "Exiting program." << endl;
+            break;
         }
 
-        // Ask user if they want to run the process again
-        cout << "Do you want to run the process again? Enter 1 to run again or 0 to exit: ";
-        cin >> choice;
+        cout << "Enter a positive exponent: ";
+        cin >> exponent;
 
-    } while (choice != 0);
-
-    cout << "GOODBYE" << endl;
+        // Since we assume exponent is always positive, no need to check for negative values
+        double power = calculatePower(base, exponent);
+        cout << base << " raised to the power of " << exponent << " is: " << power << endl;
+    }
 
     return 0;
 }
