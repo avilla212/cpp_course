d02356aac66141f4f01dea93589bb55ba4dc6bb3
unknown <alexvillafana99@gmail.com>
2024-03-03 14:31:44 -0800

    3/3 PC

--- project_2/villafana_a_project_2.cpp
+++ project_2/villafana_a_project_2.cpp
@@ -5,18 +5,30 @@ using namespace std;
 
 void residentialMenu();
 void businessMenu();
+void accountInfo(int& accountNumber, char& customerCode, int& premiumChannels, int& prePaidData);
+void outputData(int accountNumber, char customerCode, int premiumChannels, int prePaidData);
 
 int main(){
 
     bool residential = false;
     bool business = false;
+
+    int accountNumber {0};
+    char customerCode;
+    int premiumChannels {0};
+    int prePaidData {0};
+
     
     residentialMenu();
     businessMenu();
+    accountInfo(accountNumber, customerCode, premiumChannels, prePaidData);
+    outputData(accountNumber, customerCode, premiumChannels, prePaidData);
+
 
     return 0;
 }
 
+// Function to display residential menu
 void residentialMenu(){
     cout << "For residential customers, these are the rates: \n";
 
@@ -26,6 +38,7 @@ void residentialMenu(){
     cout << endl;
 }   
 
+// Function to display business menu
 void businessMenu(){
     cout << "For business customers, these are the rates: \n";
 
@@ -35,10 +48,27 @@ void businessMenu(){
     cout << endl;
 }
 
-// User input grabs int: account number, char: customer code, int: num of premium channels
-// For business customers, how much pre paid data in TB (int)
+// Function to grab account number, premium channels and for business customers, how much pre-paid data in TB
+void accountInfo(int& accountNumber, char& customerCode, int& premiumChannels, int& prePaidData){
+    cout << "Please enter your account number: ";
+    cin >> accountNumber;
 
-// Function for residential customers input
+    cout << "Please enter customer code (R for residential, B for business): ";
+    cin >> customerCode;
 
+    if (customerCode == 'R' || customerCode == 'r'){
+        cout << "PLease enter the number of premium channels: ";
+        cin >> premiumChannels;
+    } else if (customerCode == 'B' || customerCode== 'b'){
+        cout << "Please enter the amount of pre-paid data in TB:";
+        cin >> prePaidData;
+    }
+}
 
-// Function for business customers input
+// Function to output the data
+void outputData(int accountNumber, char customerCode, int premiumChannels, int prePaidData){
+    cout << "Account Number: " << accountNumber << endl;
+    cout << "Customer Code: " << customerCode << endl;
+    cout << "Premium Channels: " << premiumChannels << endl;
+    cout << "Pre-paid Data: " << prePaidData << endl;
+}
