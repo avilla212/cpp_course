312849a6be4dc63794169fb9719e5577eea2a07f
avilla212 <alexvillafana99@gmail.com>
2024-03-12 08:40:51 -0700

    3/12 mac

--- lab_7/MyInteger.h
+++ lab_7/MyInteger.h
@@ -23,8 +23,33 @@ class MyInteger {
             this->value -= value;
         }
 
+        // Multiplies the argument provided by the private data member
+        void multiply(int value){
+            this->value *= value;
+        }
+
+        /*
+            int compare(MyInteger) - accepts as an argument an object of class MyInteger, 
+            compares the integer stored in it to the integer stored in the private data member (variable) 
+            and returns an integer.  The return integer should be:
+
+            0 if  num1 < num2
+            1 if num1 = num2
+            2 if num1 > num2
+        */
+        int compare(MyInteger myInt){
+            if (value < myInt.get()){
+                return 0;
+            } else if (value == myInt.get()){
+                return 1;
+            } else {
+                return 2;
+            }
+        }
 
 
     private:
         int value;
-}
+
+            
+};
